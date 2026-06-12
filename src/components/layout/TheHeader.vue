47f2fb3de70442938ba88bbce25bc99eb83a95f3
iii <estrada.flsw@gmail.com>
2026-06-11 20:38:27 -0600

    feat – rename theme component

--- src/components/layout/TheHeader.vue
+++ src/components/layout/TheHeader.vue
@@ -1,10 +1,10 @@
 <script setup lang="ts">
-import TheTheme from "../TheTheme.vue"
+import VTheme from "../VTheme.vue"
 </script>
 
 <template>
     <header class="w-full py-4.5 sticky top-0 flex items-center justify-between gap-3 leading-0 bg-background">
         <h1 class="font-serif italic font-bold text-muted-foreground select-none"><span class="not-italic">B</span>olsillo</h1>
-        <TheTheme />
+        <VTheme />
     </header>
 </template>
